ea30484cd66ca3b5d90b736e947027b9ea79c993
Mark Basham <mark.basham@diamond.ac.uk>
2017-11-11 22:51:21 +0000

    Added proper Axis saving to the ouput file for visualisation in dawn

--- src/ARPESifier.py
+++ src/ARPESifier.py
@@ -4,8 +4,11 @@ from scipy.ndimage import zoom
 from scipy.stats import norm
 
 
-def arpesify(a, e, zoom_level=2, scale_level=0.01):
+def arpesify(a, kx, ky, kz, e, zoom_level=2, scale_level=0.01):
     b = zoom(a, (zoom_level, zoom_level, zoom_level, 1))
+    xx = zoom(kx, (zoom_level))
+    yy = zoom(ky, (zoom_level))
+    zz = zoom(kz, (zoom_level))
     v = np.zeros((b.shape[0], b.shape[1], b.shape[2], e.shape[0]))
     for i in range(b.shape[0]):
         print("i is %i" %(i))
@@ -15,16 +18,41 @@ def arpesify(a, e, zoom_level=2, scale_level=0.01):
                     v[i, j, k, :] = np.maximum(v[i, j, k, :],
                                                norm.pdf(e, loc=b[i, j, k, l],
                                                         scale=scale_level))
+
     padx = int(v.shape[0] / 2);
     pady = int(v.shape[1] / 2);
     padz = int(v.shape[2] / 2);
-    return np.pad(v, ((padx, padx), (pady, pady), (padz, padz), (0, 0)), "symmetric")
+
+    xx = np.pad(xx, ((padx, padx)), "wrap")
+    xx[:padx] -= ((xx.max()-xx.min())*(float(xx.shape[0]+1) /float(xx.shape[0])))
+    xx[-padx:] += ((xx.max()-xx.min())*(float(xx.shape[0]+1) /float(xx.shape[0])))
+
+    yy = np.pad(yy, ((pady, pady)), "wrap")
+    yy[:pady] -= ((yy.max()-yy.min())*(float(yy.shape[0]+1) /float(yy.shape[0])))
+    yy[-pady:] += ((yy.max()-yy.min())*(float(yy.shape[0]+1) /float(yy.shape[0])))
+
+    zz = np.pad(zz, ((padz, padz)), "wrap")
+    zz[:padz] -= ((zz.max()-zz.min())*(float(zz.shape[0]+1) /float(zz.shape[0])))
+    zz[-padz:] += ((zz.max()-zz.min())*(float(zz.shape[0]+1) /float(zz.shape[0])))
+
+    result = np.pad(v, ((padx, padx), (pady, pady), (padz, padz), (0, 0)), "symmetric")
+    return xx, yy, zz, result
 
 
-def save(dataset, filename):
+def save(dataset, kx, ky, kz, e, filename):
     import h5py
     handle = h5py.File(filename, 'w')
-    handle.create_dataset("data", data=dataset)
+    entry = handle.create_group('entry')
+    entry.attrs['NXclass'] = 'NXentry'
+    group = handle.create_group('data')
+    group.attrs['NXclass'] = 'NXdata'
+    group.create_dataset("data", data=dataset)
+    group.create_dataset("kx", data=kx)
+    group.create_dataset("ky", data=ky)
+    group.create_dataset("kz", data=kz)
+    group.create_dataset("Energy", data=e)
+    group.attrs['signal'] = 'data'
+    group.attrs['axes'] = 'kx,ky,kz,Energy'
     handle.close()
 
 if __name__ == "__main__":
@@ -37,11 +65,11 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--castep", action="store_true",
                         help="load castep")
     parser.add_argument("-b", "--energy_begin", type=float,
-                        help="Energy begin", default=-0.5)
+                        help="Energy begin", default=-0.2)
     parser.add_argument("-e", "--energy_end", type=float,
-                        help="Energy stop", default=0.5)
+                        help="Energy stop", default=0.05)
     parser.add_argument("-s", "--energy_steps", type=int,
-                        help="Energy steps", default=500)
+                        help="Energy steps", default=1000)
     parser.add_argument("-d", "--energy_spread", type=float,
                         help="Energy Gaussian Spread", default=0.001)
     parser.add_argument("-z", "--zoom", type=int,
@@ -54,7 +82,7 @@ if __name__ == "__main__":
         energies = np.linspace(args.energy_begin,
                                args.energy_end,
                                args.energy_steps)
-        result = arpesify(en, energies,
+        (kx, ky, kz, result) = arpesify(en, kx, ky, kz, energies,
                           zoom_level=args.zoom,
                           scale_level=args.energy_spread)
-        save(result, args.output)
+        save(result, kx, ky, kz, energies, args.output)
